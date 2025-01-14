9401a020547330fb92ae03f693c63ac183b2775c
Rebin <muhammedrebin1794@gmail.com>
2025-01-14 09:42:09 +0530

    Familiarized with packages and importing packages in js

---
+++ 10.packages/pack1.js
@@ -0,0 +1,6 @@
+
+function sampleFunction(){
+    return "This is a sample function from pack1.js"
+}
+
+export default sampleFunction;
---
+++ 10.packages/pack2.js
@@ -0,0 +1,3 @@
+import sampleFunction from "./pack1.js" // if we use export defualt in the pack1.js then {} is not needed when importing from it.
+
+console.log(sampleFunction())
