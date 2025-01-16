9c023696abcaa706376f9ff7529e60d39a033be2
Rebin <muhammedrebin1794@gmail.com>
2025-01-16 21:50:52 +0530

    Problem: is prime using js

---
+++ practice/problem.js
@@ -0,0 +1,18 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+
+    return true;
+}
+
+
+console.log(isPrime(5));
+console.log(isPrime(10));
+console.log(isPrime(17));
+console.log(isPrime(21));
